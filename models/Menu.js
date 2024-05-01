25a12030b8976d86f751c35928f76b81666595d6
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-01 15:56:41 -0400

    create a menu model

--- models/Menu.js
+++ models/Menu.js
@@ -1,6 +1,10 @@
-const {sequelize} = require('../db');
-const { Sequelize } = require('sequelize');
+const {db} = require('../db');
+const { Sequelize, DataTypes } = require('sequelize');
 
 // TODO - create a Menu model
-
+const Menu = db.define('menu', {
+  title: {
+    type: DataTypes.STRING
+  }
+})
 module.exports = {Menu};
