7736142ad67245d829453a8c94985dfcc3184ff6
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-01 15:46:42 -0400

    add db config

--- db.js
+++ db.js
@@ -3,6 +3,12 @@ const { Sequelize } = require('sequelize');
 
 // TODO - connect to db via sequelize
 
+const db = new Sequelize({
+  dialect: 'sqlite',
+  storage: './db.sqlite',
+});
+
 module.exports = {
-    sequelize
+    // sequelize,
+    db
 };
