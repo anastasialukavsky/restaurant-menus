69701caba18f14ef4e04822c018f37102c5722ef
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-01 15:57:34 -0400

    add test cases

--- index.test.js
+++ index.test.js
@@ -1,43 +1,96 @@
-const {sequelize} = require('./db')
-const {Restaurant, Menu} = require('./models/index')
-const {
-    seedRestaurant,
-    seedMenu,
-  } = require('./seedData');
+const { db } = require('./db');
+const { Restaurant, Menu } = require('./models/index');
+const { seedRestaurant, seedMenu } = require('./seedData');
 
 describe('Restaurant and Menu Models', () => {
-    /**
-     * Runs the code prior to all tests
-     */
-    beforeAll(async () => {
-        // the 'sync' method will create tables based on the model class
-        // by setting 'force:true' the tables are recreated each time the 
-        // test suite is run
-        await sequelize.sync({ force: true });
+
+  beforeAll(async () => {
+
+    await db.sync({ force: true });
+     await Restaurant.bulkCreate(seedRestaurant);
+     await Menu.bulkCreate(seedMenu);
+  });
+
+  test('can create a Restaurant', async () => {
+    const createdRestaurant = await Restaurant.create(seedRestaurant[0]);
+
+    const foundRestaurant = await Restaurant.findOne({
+      where: { id: createdRestaurant.id },
     });
 
-    test('can create a Restaurant', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
+    expect(foundRestaurant).toBeTruthy();
+    expect(foundRestaurant.name).toEqual(seedRestaurant[0].name);
+    expect(foundRestaurant.location).toEqual(seedRestaurant[0].location);
+    expect(foundRestaurant.cuisine).toEqual(seedRestaurant[0].cuisine);
+  });
 
-    test('can create a Menu', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
+  test('can create a Menu', async () => {
+    const createdMenu = await Menu.create(seedMenu[0]);
 
-    test('can find Restaurants', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
+    const foundMenu = await Menu.findOne({ where: { id: createdMenu.id } });
 
-    test('can find Menus', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
+    expect(foundMenu).toBeTruthy();
+    expect(foundMenu.title).toEqual(seedMenu[0].title);
+  });
 
-    test('can delete Restaurants', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
-})
+  test('can find Restaurants', async () => {
+        const allRestaurants = await Restaurant.findAll();
+
+        const sortedRestaurants = allRestaurants.sort((a, b) => a.id - b.id);
+
+        expect(
+          sortedRestaurants.map((restaurant) => ({
+            name: restaurant.name,
+            location: restaurant.location,
+            cuisine: restaurant.cuisine,
+          }))
+        ).toEqual(expect.arrayContaining(seedRestaurant));
+
+        expect(seedRestaurant).toEqual(
+          expect.arrayContaining(
+            sortedRestaurants.map((restaurant) => ({
+              name: restaurant.name,
+              location: restaurant.location,
+              cuisine: restaurant.cuisine,
+            }))
+          )
+        );
+  });
+
+  test('can find Menus', async () => {
+    const allMenus = await Menu.findAll();
+
+    const sortedMenus = allMenus.sort((a, b) => a.id - b.id);
+
+    expect(
+      sortedMenus.map((menu) => ({
+        title: menu.title,
+      }))
+    ).toEqual(expect.arrayContaining(seedMenu));
+
+    expect(seedMenu).toEqual(
+      expect.arrayContaining(
+        sortedMenus.map((menu) => ({
+          title: menu.title,
+        }))
+      )
+    );
+  });
+
+  test('can delete Restaurants', async () => {
+    const restaurantToDelete = await Restaurant.findOne({ where: { id: 1 } });
+
+    await restaurantToDelete.destroy();
+
+    const deletedRestaurant = await Restaurant.findOne({ where: { id: 1 } });
+    expect(deletedRestaurant).toBeNull();
+  });
+  test('can delete Menus', async () => {
+    const menuToDelete = await Menu.findOne({ where: { id: 1 } });
+
+    await menuToDelete.destroy();
+
+    const deletedMenu= await Menu.findOne({ where: { id: 1 } });
+    expect(deletedMenu).toBeNull();
+  });
+});
