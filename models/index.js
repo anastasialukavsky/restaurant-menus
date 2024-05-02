c9ad5ef1adaf5773ce69b64df51bb1d5e1f5032e
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-02 14:37:01 -0400

    define associations

--- models/index.js
+++ models/index.js
@@ -1,4 +1,12 @@
 const {Restaurant} = require('./Restaurant')
 const {Menu} = require('./Menu')
+const Item = require('./Item')
 
-module.exports = { Restaurant, Menu }
+Restaurant.hasMany(Menu)
+Menu.belongsTo(Restaurant)
+
+Menu.belongsToMany(Item, { through: 'MenuItems' }); 
+Item.belongsToMany(Menu, { through: 'MenuItems' });
+
+
+module.exports = { Restaurant, Menu, Item }
